a01d11871bc455bc768a84a6702e1f59d49b4677
haleykelling <haley@kellings.com>
2020-07-28 15:52:13 -0600

    Add login method to Authentication controller with error handling. Add authenticate method to application controller to verify authentication before interacting with habits.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+
+    def authenticate
+        header = request.headers["Authorization"]
+        token = header.split(" ").last
+
+        if !token 
+            render json: {errors: ["Must be logged in."]}, status: :forbidden
+        else
+            secret = Rails.application.secret_key_base
+            begin
+                payload = JWT.decode(token, secret)[0] 
+                @user = User.find(payload[“user_id”]
+            rescue
+                render json: {errors: ["Must be logged in."]}, status: :forbidden
+            end
+        end
+    end
+
 end
---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,18 @@
+class AuthenticationController < ApplicationController
+
+    def login
+        @user = User.find_by(email: params[:user][:email])
+        user_info = {name: @user.first_name, email: @user.email}
+        
+        if !@user || !@user.authenticate(params[:user][:password])
+            render json: {errors: @user.errors.full_messages}, status: :unauthorized
+        else
+            secret = Rails.application.secret_key_base
+            payload = {user_id: @user.id}
+            @token = JWT.encode payload, secret 
+            render json: {user: user_info, token: @token} 
+        end
+        
+    end
+
+end
