3d410bbafbaf02149497fc13e8a1bfe7b45aead5
haleykelling <haley@kellings.com>
2020-07-28 13:55:35 -0600

    User resource created. Strong params and model validations added to user. Create method established for user with error handling. Token generated upon sign up.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+    
+    def create
+        @user = User.new user_params
+        if @user.save
+            secret = Rails.application.secret_key_base
+            payload = {user_id: @user.id}
+            @token = JWT.encode payload, secret 
+            render json: {token: @token, user: @user}
+        else
+            render json: {errors: @user.errors.full_messages}, status: :not_acceptable
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(
+            :first_name,
+            :last_name,
+            :email,
+            :password
+        )
+    end
+
+
+end
