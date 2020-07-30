6dc10506ae4d2ad74d96c72b27c565c5aecfc1ef
haleykelling <haley@kellings.com>
2020-07-30 12:18:15 -0600

    Edited seeds to have streak info.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,5 @@ Habit.destroy_all
 Habit.create(name: "Drink Water", details: "Drink 2 liters of water per day", user_id: 1, date_last_completed: "2020-07-16", current_streak: 5, longest_streak: 5, total: 5)
 Habit.create(name: "Walk", details: "Walk outside for 30 minutes every day", user_id: 1, date_last_completed: "2020-07-29", current_streak: 4, longest_streak: 5, total: 15)
 Habit.create(name: "Meditate", details: "Meditate for 10 minutes", user_id: 1, date_last_completed: "2020-07-29", current_streak: 3, longest_streak: 3, total: 4)
-Habit.create(name: "Read More", details: "Read for at least 15 minutes every day before going to sleep", user_id: 1)
-Habit.create(name: "Don't Snooze", details: "Wake up on the first alarm without snoozing!", user_id: 1)
+Habit.create(name: "Read More", details: "Read for at least 15 minutes every day before going to sleep", user_id: 1, date_last_completed: "2020-07-19", current_streak: 15, longest_streak: 21, total: 55)
+Habit.create(name: "Don't Snooze", details: "Wake up on the first alarm without snoozing!", user_id: 1, date_last_completed: "2020-07-29", current_streak: 11, longest_streak: 11, total: 30)
